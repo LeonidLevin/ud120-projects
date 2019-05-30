c9e351b58a7b2a180fcd0206297a03f8f73fd7ed
Leonid Levin <leonid@hetnet.nl>
2019-05-30 13:28:46 +0200

    explore enron data

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -17,6 +17,29 @@
 
 import pickle
 
-enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
+enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+print len(enron_data)
+print len(enron_data["SKILLING JEFFREY K"])
 
+poiCount = 0
+for person in enron_data:
+    if enron_data[person]["poi"] == 1: 
+        poiCount+=1
+print poiCount
+
+print len([person for person in enron_data if enron_data[person]["poi"]==1])
+
+print enron_data["PRENTICE JAMES"]["total_stock_value"]
+
+print enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]
+
+print enron_data["SKILLING JEFFREY K"]["exercised_stock_options"]
+
+print enron_data["SKILLING JEFFREY K"]['total_payments'], enron_data["LAY KENNETH L"]['total_payments'], enron_data["FASTOW ANDREW S"]['total_payments']
+
+print len([person for person in enron_data if enron_data[person]["salary"]!='NaN'])
+print len([person for person in enron_data if enron_data[person]["email_address"]!='NaN'])
+
+print len([person for person in enron_data if enron_data[person]['total_payments'] == 'NaN']) #* 1.0 / len(enron_data)
+print len([person for person in enron_data if enron_data[person]['total_payments'] == 'NaN' and enron_data[person]["poi"]==1])
