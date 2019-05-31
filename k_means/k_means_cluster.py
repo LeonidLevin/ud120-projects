27dc65332edd663a083fc82f232f7a8981d45837
Leonid Levin <leonid@hetnet.nl>
2019-05-31 13:39:14 +0200

    feature scaling

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -56,17 +56,22 @@ feature_1 = "salary"
 feature_2 = "exercised_stock_options"
 feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2, feature_3]
+features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+finance_features = scaler.fit_transform(finance_features)
+print "scaled values of salary 200000 and exercised stock options of 1M:", scaler.transform([[200000.0, 1000000.0]])
 
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2, f3 in finance_features:
-    plt.scatter( f1, f2, f3 )
+for f1, f2 in finance_features:
+    plt.scatter( f1, f2 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
