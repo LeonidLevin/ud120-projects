08bbb14defe9d25315e058fd0ec86765a3d36231
Leonid Levin <leonid@hetnet.nl>
2019-06-02 12:04:51 +0200

    feature selection

--- feature_selection/find_signature.py
+++ feature_selection/find_signature.py
@@ -19,8 +19,8 @@ authors = pickle.load( open(authors_file, "r") )
 ### remainder go into training)
 ### feature matrices changed to dense representations for compatibility with
 ### classifier functions in versions 0.15.2 and earlier
-from sklearn import cross_validation
-features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+from sklearn import model_selection
+features_train, features_test, labels_train, labels_test = model_selection.train_test_split(word_data, authors, test_size=0.1, random_state=42)
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
@@ -38,6 +38,17 @@ labels_train   = labels_train[:150]
 
 
 ### your code goes here
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier(min_samples_split=5)
+clf.fit(features_train, labels_train)
+print "features:", len(features_train[0])
+print "accuracy on training set:", clf.score(features_train, labels_train)
+print "accuracy on testing set:", clf.score(features_test, labels_test)
+
+important_features = [(ii, clf.feature_importances_[ii]) for ii in range(0, len(clf.feature_importances_)) if clf.feature_importances_[ii] > 0.2]
+print important_features
+
+print "signagure word: ", vectorizer.get_feature_names()[important_features[0][0]]
 
 
 
